23ee68b6ac1178ee593f373f83dca89b4f9b7ea4
Sasith-Ruvin <cb011152@students.apiit.lk>
2024-10-16 22:28:48 +0530

    Added EmailJS

--- src/components/Forms/ContactForm.tsx
+++ src/components/Forms/ContactForm.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import image from '../../Images/ProductImages/coffee2.webp';
+import emailjs from 'emailjs-com';
 
 export default function ContactForm() {
+  const form = useRef<HTMLFormElement | null>(null);
+
+  const sendEmail = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (form.current) {
+      emailjs
+        .sendForm(
+          'service_nqsm0xo', //service ID
+          'template_yx5dbae', //template ID
+          form.current,
+          'XnoWvLpePBYR2r11y' // public key (user ID)
+        )
+        .then(
+          (result) => {
+           
+            alert('Message sent successfully!');
+          },
+          (error) => {
+            alert('An error occurred, please try again.');
+          }
+        );
+    }
+  };
   return (
     <div className='flex justify-center py-10 px-5'>
       <div className='flex max-w-6xl w-full bg-white rounded-lg shadow-lg overflow-hidden'>
@@ -14,12 +39,13 @@ export default function ContactForm() {
         <div className='w-1/2 bg-gray-100 p-8 flex flex-col justify-center'>
           <h2 className='text-3xl font-bold mb-6'>Get in Touch</h2>
 
-          <form action="#" className='space-y-4'>
+          <form ref={form} onSubmit={sendEmail} className='space-y-4'>
             {/* Name Input */}
             <div>
               <label htmlFor="name" className='block text-sm font-medium text-gray-700'>Name</label>
               <input 
                 type="text"
+                name='name'
                 placeholder='Your Name'
                 className='w-full mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500'
               />
@@ -30,6 +56,7 @@ export default function ContactForm() {
               <label htmlFor="email" className='block text-sm font-medium text-gray-700'>E-mail</label>
               <input 
                 type="text"
+                name='email'
                 placeholder='Your Email'
                 className='w-full mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500'
               />
@@ -40,6 +67,7 @@ export default function ContactForm() {
               <label className="block text-sm font-medium text-gray-700">Subject</label>
               <input
                 type="text"
+                name='subject'
                 placeholder="Title.."
                 className="w-full mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
               />
@@ -49,6 +77,7 @@ export default function ContactForm() {
             <div>
               <label className="block text-sm font-medium text-gray-700">Message</label>
               <textarea
+              name='message'
                 placeholder="Type Your Message here..."
                 className="w-full mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 h-32"
               ></textarea>
